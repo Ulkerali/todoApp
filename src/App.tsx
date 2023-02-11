e6f2a0d80224958b8d4c427f8462eb843024cdc8
Ali Ulker <ulkerali1453@gmail.com>
2023-02-11 19:23:51 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import ListCard from './components/ListCard
+
+const App = () => {
+  return (
+    <View>
+      <ListCard />
+      asdasdasdsad
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ src/components/ListCard/ListCard.js
@@ -0,0 +1,18 @@
+import {StyleSheet, Text, View, FlatList, TouchableOpacity} from 'react-native';
+import React from 'react';
+
+const ListCard = () => {
+  return (
+    <View>
+      <View>
+        <Text>YAPILACAKLAR</Text>
+        <Text></Text>
+      </View>
+      <View></View>
+    </View>
+  );
+};
+
+export default ListCard;
+
+const styles = StyleSheet.create({});
